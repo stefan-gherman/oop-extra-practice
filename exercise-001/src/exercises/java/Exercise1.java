4852e13d8d88540cafd224e6caaf06bcbc65b5f2
Stefan Gherman <scgheman@gmail.com>
2020-04-20 20:42:17 +0300

    Add first two exercises Java.

---
+++ exercise-001/src/exercises/java/Exercise1.java
@@ -0,0 +1,7 @@
+package exercises.java;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Exercise1 executed");
+    }
+}
---
+++ exercise-002/exercises/java/Exercise2.java
@@ -0,0 +1,9 @@
+package exercises.java;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int num1 = 3, num2= 15;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
