8714d1517cc1cab9de462cc53b8b36b8ffc8dbdb
Stefan Gherman <scgheman@gmail.com>
2020-04-22 04:57:09 +0300

    Add exercise 3 C#

---
+++ exercise-003/Exercise3/CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise3.CSharp
+{
+    class Exercise3
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            int sum = 0;
+            foreach (int number in arr)
+            {
+                sum += number;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
